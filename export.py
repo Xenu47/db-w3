65e969f97d45c4055bf5df0c5faf3a708a856d2f
Anton Storozhuk <44733274+Xenu47@users.noreply.github.com>
2020-05-04 01:57:42 +0300

    Add files via upload

---
+++ export.py
@@ -0,0 +1,30 @@
+import cx_Oracle
+import csv
+
+username = "Xenu"
+password = "pass"
+database = "localhost"
+connection = cx_Oracle.connect(username, password, database, encoding='UTF-8') #ãããããããããããã
+cursor = connection.cursor()
+
+table_list = ['Pricing', 'Type_', 'App_', 'Category', 'Genre']
+
+try:
+    for table in table_list:
+        with open(table + '.csv', 'w', newline='', encoding='UTF-8') as file:
+            query = "SELECT * FROM " + table
+            cursor.execute(query)
+            row = cursor.fetchone()
+
+            cols = []
+            for k in cursor.description:
+                cols.append(k[0])
+            csv_writer = csv.writer(file, delimiter=',')
+            csv_writer.writerow(cols)
+
+            while row:
+                csv_writer.writerow(row)
+                row = cursor.fetchone()
+finally:
+    cursor.close()
+    connection.close()
