0953d76c061579bdde8608b4c6ee6667017cb0d5
Anton Storozhuk <44733274+Xenu47@users.noreply.github.com>
2020-05-21 19:30:25 +0300

    Add files via upload

---
+++ populate.sql
@@ -0,0 +1,15 @@
+INSERT INTO Category(app_category)
+VALUES('GAME');
+INSERT INTO Category(app_category)
+VALUES('TOOLS');
+INSERT INTO Category(app_category)
+VALUES('FAMILY');
+
+INSERT INTO Genre(app_genre)
+VALUES('Action');
+INSERT INTO Genre(app_genre)
+VALUES('Card');
+INSERT INTO Genre(app_genre)
+VALUES('Puzzle');
+INSERT INTO Genre(app_genre)
+VALUES('Medical');
